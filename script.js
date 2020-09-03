3d48e3ef28ff1bb9c145b4821ffe49ba04ebca44
Insomnia <jamie.mar.jones14@gmail.com>
2020-09-03 00:02:51 -0400

    coding complete!

--- script.js
+++ script.js
@@ -9,40 +9,57 @@ function writePassword() {
   passwordText.value = password;
 }
 
-var password = "";
-var passwordLength = "";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-var upperCase = "ABCDEFGHIJKLMNPOQRSTUVWXYZ".split("");
-var symbols = "!@#$%^&*()".split("");
-var num = "1234567890".split("");
-
 // console.log(generatePassword);
 
 function generatePassword() {
+  var password = "";
+  var passwordLength = "";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  var upperCase = "ABCDEFGHIJKLMNPOQRSTUVWXYZ".split("");
+  var symbols = "!@#$%^&*()".split("");
+  var num = "1234567890".split("");
+  var totalChoice = [];
   var passwordLength = prompt(
     "How many characters do you want in your password? (Note: Must be between 8 to 128 characters)"
   );
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please select a number between 8 and 128.");
+    generatePassword();
+  } else {
+    var passwordNum = confirm("Would you like to include numbers?");
+    if (passwordNum === true) {
+      totalChoice = [...totalChoice, ...num];
+    }
+    var passwordLow = confirm("Would you like to include lowercase letters?");
+    if (passwordLow === true) {
+      totalChoice = [...totalChoice, ...lowerCase];
+    }
+    var passwordUp = confirm("Would you like to include uppercase numbers?");
+    if (passwordUp === true) {
+      totalChoice = [...totalChoice, ...upperCase];
+    }
+    var passwordChar = confirm("Would you like to include special characters?");
+    if (passwordChar === true) {
+      totalChoice = [...totalChoice, ...symbols];
+    }
+    if (
+      passwordNum === false &&
+      passwordLow === false &&
+      passwordChar === false &&
+      passwordUp === false
+    ) {
+      alert("Must choose a character.");
+      generatePassword();
+    } else {
+      for (i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * totalChoice.length);
+        password = password + totalChoice[randomIndex];
+      }
+      return password;
+    }
+
+    // TODO: Generate a real password and replace the return string with a real string.
   }
-  var passwordNum = confirm("Would you like to include numbers?");
-
-  var passwordLow = confirm("Would you like to include lowercase letters?");
-
-  var passwordUp = confirm("Would you like to include uppercase numbers?");
-
-  var passwordChar = confirm("Would you like to include special characters?");
-
-  //
-  // } else if (wantLower) {
-  //   var wantUpper = confirm("Do you want to include uppercase letters?");
-  // } else if (wantNum) {
-  //   var wantNum = confirm("Do you want to include numbers?");
-  // } else if (wantChar) {
-  //   var wantNum = confirm("Do you want to include special characters?");
-
-  // TODO: Generate a real password and replace the return string with a real string.
-  return password;
 }
 // ALL CODE GOES IN HERE
 
